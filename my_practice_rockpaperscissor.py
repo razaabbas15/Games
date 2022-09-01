7d26f5cfb7b50e302eac7a36870cf07b59598218
razaabbas15 <107624847+razaabbas15@users.noreply.github.com>
2022-09-01 18:14:30 +0500

    Add files via upload

---
+++ my_practice_rockpaperscissor.py
@@ -0,0 +1,29 @@
+from random import randint 
+game_contents = ["Rock","Paper","Scissor"]
+opponent = game_contents[randint(0,2)]
+me = False
+
+while me == False:
+    me = input("Rock,Paper,Scissor?")
+    if me == opponent:
+        print("Tie!")
+    elif me == "Rock":
+        if opponent == "Paper":
+            print("You lose!" , opponent ,"covers ", me)
+        else:
+            print("You win!", me ,"Smashed", opponent)
+    elif me == "Scissor":
+        if opponent == "Rock":
+            print("You lose!", opponent ,"Smashed",me)
+        else:
+            print("You win!", me ,"Cutts",opponent)
+    elif me =="Paper":
+        if opponent == "Scissor":
+            print("You lose!",opponent ,"Cutts", me)
+        else:
+            print("You win!", me ,"covers",opponent)     
+    else:
+        print("Check Spelling!")
+        
+    me = False
+    computer = game_contents[randint(0,2)]
